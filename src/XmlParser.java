e78ed8ab2c07d40dac93e8865086fb8bb84c653a
Marcell <misomarcell@gmail.com>
2017-02-07 14:54:18 +0100

    kecske

--- src/XmlParser.java
+++ src/XmlParser.java
@@ -1,6 +1,24 @@
-public class XmlParser {
-    public void LoadXmlDocument(String fullPath)
-    {
+import java.io.File;
 
-    }
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+
+public class XmlParser {
+	public void LoadXmlDocument(String fullPath) {
+
+		try {
+			File xmlFile = new File(fullPath);
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(xmlFile);
+
+			doc.getDocumentElement().normalize();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
 }
